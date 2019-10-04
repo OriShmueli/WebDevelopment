b15fb804d86da8c0896b85b284a930ba81907386
OriShmueli <43569659+OriShmueli@users.noreply.github.com>
2019-10-04 14:12:22 -0700

    i thinks im all most there

--- js_vanilla/main.js
+++ js_vanilla/main.js
@@ -1,20 +1,35 @@
 function CreateNewComment(){
-    
-    let CommentTable = document.getElementById("CommentTable")
-    
-    let NewComment = CommentTable.insertRow(-1)
+
+    let body = document.getElementsByTagName("body")[0]
+    let CommentTable = document.createElement("table")
+    CommentTable.setAttribute("class", "CommentTable");
+
+    let NewComment = document.createElement("tr")
     NewComment.setAttribute("class", "Comment")
 
-    let UserCommentImage = NewComment.insertCell(0)
+    let UserCommentImage = document.createElement("td")
     UserCommentImage.setAttribute("class", "ProfileCommentPic")
+    
     let img = document.createElement("img")
     img.src = "aaa"
     img.setAttribute("class", "ProfileCommentPic")
-    
 
-    let UserCommentText = NewComment.insertCell(1);
+    let UserCommentText = document.createElement("td")
     UserCommentText.setAttribute("class", "CommentText")
     
-    UserCommentImage.innerHTML = img
-    UserCommentText.innerHTML = "Test text"
+    let CommentText = document.getElementsByClassName("CommentText").
+
+    NewComment.appendChild(UserCommentImage)
+    NewComment.appendChild(UserCommentText)
+    CommentTable.appendChild(NewComment)
+    body.appendChild(CommentTable)
+    
+    UserCommentImage = img
+    if(CommentText == ""){
+        alert("plz enter some thing to send an comment")
+    }
+    else{
+        UserCommentText.innerHTML = CommentText
+    }
 }
+
